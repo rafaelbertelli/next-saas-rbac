7e81405c777caa70b443df9545158aae272758b0
Rafael Borges <rafael.borges@mercadolivre.com>
2025-05-14 23:32:59 -0300

    chore(api): replace jest-config with custom Jest configuration and update package.json dependencies for Jest and ts-jest

--- apps/api/jest.config.ts
+++ apps/api/jest.config.ts
@@ -1,5 +1,19 @@
-import baseConfig from "@repo/jest-config";
+import type { Config } from "jest";
 
-export default {
-  ...baseConfig,
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src"],
+  moduleFileExtensions: ["ts", "js", "json"],
+  testMatch: ["**/*.spec.ts"],
+  collectCoverageFrom: ["src/**/*.{ts,js}"],
+  coverageDirectory: "coverage",
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
 };
+
+export default config;
